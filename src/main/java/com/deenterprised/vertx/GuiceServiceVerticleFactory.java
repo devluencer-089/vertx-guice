5890de972ff94590423b40ab20ee34ee8fc09ec3
Michael Omann <michael.omann@gmail.com>
2015-03-08 23:52:41 +0100

    add a workaround for bug in verticle deployment

---
+++ src/main/java/com/deenterprised/vertx/GuiceServiceVerticleFactory.java
@@ -0,0 +1,48 @@
+package com.deenterprised.vertx;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Verticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.spi.VerticleFactory;
+import io.vertx.service.ServiceVerticleFactory;
+
+public class GuiceServiceVerticleFactory implements VerticleFactory {
+
+    private final VerticleFactory delegate = new ServiceVerticleFactory();
+
+
+    @Override
+    public int order() {
+        return delegate.order();
+    }
+
+    @Override
+    public boolean requiresResolve() {
+        return delegate.requiresResolve();
+    }
+
+    @Override
+    public String resolve(String identifier, DeploymentOptions deploymentOptions, ClassLoader classLoader) throws Exception {
+        return "java:" + delegate.resolve(identifier, deploymentOptions, classLoader);
+    }
+
+    @Override
+    public void init(Vertx vertx) {
+        delegate.init(vertx);
+    }
+
+    @Override
+    public void close() {
+        delegate.close();
+    }
+
+    @Override
+    public String prefix() {
+        return delegate.prefix();
+    }
+
+    @Override
+    public Verticle createVerticle(String verticleName, ClassLoader classLoader) throws Exception {
+        return delegate.createVerticle(verticleName, classLoader);
+    }
+}
