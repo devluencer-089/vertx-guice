b5772638a7358b63c2c9971b7a41fc251a560fca
Michael Omann <michael.omann@gmail.com>
2015-03-09 00:34:31 +0100

    add rx to the mix

--- src/main/java/com/deenterprised/vertx/VertxModule.java
+++ src/main/java/com/deenterprised/vertx/VertxModule.java
@@ -6,13 +6,16 @@ import io.vertx.core.Vertx;
 class VertxModule extends AbstractModule {
 
     private final Vertx vertx;
+    private final io.vertx.rxjava.core.Vertx rxVertx;
 
     VertxModule(Vertx vertx) {
         this.vertx = vertx;
+        this.rxVertx = new io.vertx.rxjava.core.Vertx(vertx);
     }
 
     @Override
     protected void configure() {
         bind(Vertx.class).toInstance(vertx);
+        bind(io.vertx.rxjava.core.Vertx.class).toInstance(rxVertx);
     }
 }
